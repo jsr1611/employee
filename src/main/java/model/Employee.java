b2c21e8bf12dad827602e92145068fe2f346346d
Jimmy <jsr1611@outlook.com>
2021-12-09 14:42:54 +0900

    some updates in entity class

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -6,5 +6,23 @@ package model;
  * created:  09/12/2021 2:29 PM
  */
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
 }
