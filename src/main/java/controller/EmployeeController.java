48f87add3243bc32b50b8a98f3c86274a7b2e038
Jimmy <jsr1611@outlook.com>
2021-12-09 14:39:13 +0900

    Created model and controller classes

---
+++ src/main/java/controller/EmployeeController.java
@@ -0,0 +1,9 @@
+package controller;
+/*
+ * project:  employee
+ * author:   Administrator
+ * created:  09/12/2021 2:38 PM
+ */
+
+public class EmployeeController {
+}
---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,10 @@
+package model;
+
+/*
+ * project:  employee
+ * author:   Jumanazar Said
+ * created:  09/12/2021 2:29 PM
+ */
+
+public class Employee {
+}
