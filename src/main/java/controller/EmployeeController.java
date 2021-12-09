4fa9292e5c5e8d80861923ec153fe3619e68fe8a
Jimmy <jsr1611@outlook.com>
2021-12-09 14:40:36 +0900

    some updates in controller

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,9 +1,15 @@
 package controller;
 /*
  * project:  employee
- * author:   Administrator
+ * author:   Jumanazar Said
  * created:  09/12/2021 2:38 PM
  */
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeController {
+
 }
